899268712fbf04a9df4249405cd0f41151e8bb32
Damien Asseya <dasseya1@gmail.com>
2017-03-15 19:11:42 +0000

    Improved homepage and added comments to functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,45 @@
+//These are global variables
+
 var number = 30;
 var intervalId;
+var gcount = 0;
+var wcount = 0;
+var unanswered = 0;
 
+//Start the coundown
 function run() {
     intervalId = setInterval(decrement, 1000);
 }
 
+//Hide the questions and other contents
 $(window).on("load", hide);
 
 $('#start').on('click', function(){
     $(this).hide();
     show();
+    run();
 });
+
+$('#done').on('click', function(){
+    $('#start').hide();
+    hide();
+    rSummary();
+    stop();
+});
+
+//Create the elements for the result page
+function rSummary(){
+    var alldone = $('<h2>').html('All Done!');
+    var canswers = $('<p>').html('Correct answers: ' + gcount);
+    var wanswers = $('<p>').html('Incorrect answers: ' + wcount);
+    var cunanswered = $('<p>').html('Unanswered: ' + unanswered);
+    var newclass= $('<div class="col-lg-4 col-lg-offset-4 text-center">');
+    newclass.append(alldone);
+    newclass.append(canswers);
+    newclass.append(wanswers);
+    newclass.append(cunanswered);
+    $('.row:nth(2)').append(newclass);
+}
 
 function decrement() {
     //  Decrease number by one.
@@ -26,6 +55,9 @@ function decrement() {
       //  Once number hits zero...
     else if (number === 0) {
         //  ...run the stop function.
+        $('#start').hide();
+        hide();
+        rSummary();
         stop();
     }
 }
@@ -33,8 +65,8 @@ function decrement() {
 function stop() {
     clearInterval(intervalId);
 }
-    //  Execute the run function.
-run();
+
+
 
 //This function will hide some contents
 function hide(){
@@ -49,3 +81,10 @@ function show() {
     $('#time').show();
     $('#done').show();
 }
+
+//Grab all radio buttons and calculate good and incorrect answers when a change occurs
+$('input[type=radio]').on("change", function() {
+   gcount =  $('input[value=goodanswer]:checked').length;
+   wcount = $('input[value=wrong]:checked').length;
+   unanswered = (7-(gcount + wcount));
+});
